b7d603e42854e5143e0d5583d79664e6225693da
saepulfariz <saepulfariz108@gmail.com>
2024-08-21 21:07:17 +0700

    add connection mysql

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/flask_mvc_orm'
+# https://medium.com/@alfatihridhont/flask-chapter-4-database-orm-786f16fd3b59
+
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
