28aa142f78f21521bdd23bacffe0c86d98329f87
saepulfariz <saepulfariz108@gmail.com>
2024-08-21 21:17:44 +0700

    change column product

--- app.py
+++ app.py
@@ -14,3 +14,8 @@ migrate = Migrate(app, db)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
+    
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    price = db.Column(db.Float(13))
