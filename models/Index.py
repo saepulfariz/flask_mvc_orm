658c160fe71c3f6b305961173b642420cec78d0f
saepulfariz <saepulfariz108@gmail.com>
2024-08-22 13:44:27 +0700

    add relation user and product

--- models/Index.py
+++ models/Index.py
@@ -21,8 +21,15 @@ class User(BaseTable):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=False, nullable=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     age = db.Column(db.Float(13))
     address = db.Column(db.String(120))
+
+    # Relationship: One user can have many products
+    products = db.relationship('Products', backref='owner', lazy=True)
+
+
     @property
     def serialize(self):
         return {
@@ -36,9 +43,14 @@ class User(BaseTable):
         }
     
 class Product(BaseTable):
+    __tablename__ = 'products'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
     price = db.Column(db.Float(13))
+
+    # Foreign key to associate product with user
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
     @property
     def serialize(self):
         return {
