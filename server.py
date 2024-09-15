882dab6d58c3cb1e89f5d0f0d3646f55706fea5b
saepulfariz <saepulfariz108@gmail.com>
2024-09-15 21:40:26 +0700

    add env app

--- server.py
+++ server.py
@@ -6,6 +6,16 @@ from flask_migrate import Migrate
 
 app = create_app(Config)
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+app_host = os.environ['FLASK_RUN_HOST']
+app_port = os.environ['FLASK_RUN_PORT']
+app_debug = os.environ['FLASK_DEBUG']
+app_name = os.environ['FLASK_APP']
+app_reverse_proxy = os.environ['FLASK_REVERSE_PROXY']
+
 migrate = Migrate() #define migration
 
 migrate.init_app(app,db) #initiate migration
@@ -16,4 +26,5 @@ if __name__ == '__main__':
     # with app.app_context():
     #     # Inisialisasi database atau tugas lain yang membutuhkan app context
     #     db.create_all()
-    app.run(debug=True)
+
+    app.run(app_host, app_port, app_debug)
