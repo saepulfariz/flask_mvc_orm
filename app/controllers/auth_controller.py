2bc0eeb0b433d44488673571886970443174da0a
saepulfariz <saepulfariz108@gmail.com>
2024-09-18 15:34:55 +0700

    add form field validation auth login

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -1,7 +1,20 @@
 from flask import request, redirect, url_for, render_template, flash
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length
+
+class LoginFrom(FlaskForm):
+    username = StringField('Username', 
+                           validators=[DataRequired(message="Username tidak boleh kosong."), Length(min=3, max=50,message="Username harus antara 3 sampai 50 karakter.")], 
+                           render_kw={"class": "form-control", "placeholder": "Enter your username/Email"})
+    password = PasswordField('Password',
+                        render_kw={"class": "form-control", "placeholder": "Enter your password"})
+    submit = SubmitField('Submit', render_kw={"class": "btn btn-primary"})
+
 def index() :
+    form = LoginFrom()
     data = {
         'title' : 'Login'
     }
-    return render_template('auth/login.html', data=data) 
+    return render_template('auth/login.html', data=data, form=form) 
