69e9211ca51c250b71b446a54ddf6efec1dc94ad
saepulfariz <saepulfariz108@gmail.com>
2024-09-20 09:24:29 +0700

    add field role_id in table users and fix load role_model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,4 @@ from .user_model import User
 from .product_model import Product
 from .stundent_model import Student
 from .pcs_model import PcsModel
+from .role_model import Role
--- app/models/user_model.py
+++ app/models/user_model.py
@@ -7,6 +7,7 @@ class User(BaseModel):
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id', onupdate='CASCADE', ondelete='SET NULL'), nullable=True)
     
     # Relationship: One user can have many products
     # products = db.relationship('Product', backref='owner', lazy=True)
