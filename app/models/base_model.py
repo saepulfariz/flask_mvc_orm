485533de21756272651cb55436fc7e22c6775551
saepulfariz <saepulfariz108@gmail.com>
2024-09-22 19:53:22 +0700

    set time zone created_at and updated_at GTM+7

--- app/models/base_model.py
+++ app/models/base_model.py
@@ -1,11 +1,15 @@
 from datetime import datetime
+from pytz import timezone
 from app import db
+
+# Zona waktu GMT+7
+tz = timezone('Asia/Jakarta')
 
 class BaseModel(db.Model):
     __abstract__ = True  # Ini membuat SQLAlchemy tidak membuat tabel untuk BaseModel
 
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(tz))
+    updated_at = db.Column(db.DateTime, default=lambda: datetime.now(tz), onupdate=lambda: datetime.now(tz))
 
     def __repr__(self):
         return f"<{self.__class__.__name__} created_at={self.created_at} updated_at={self.updated_at}>"
