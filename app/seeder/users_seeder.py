744c49ee176b6b56b1e57cbda3a9a88c4081df34
saepulfariz <saepulfariz108@gmail.com>
2024-09-26 17:51:02 +0700

    add seeder user admin

---
+++ app/seeder/users_seeder.py
@@ -0,0 +1,17 @@
+from app import db
+from app.models import User
+from passlib.hash import pbkdf2_sha256
+
+def run():
+    name = 'admin'
+    username = 'admin'
+    email = 'admin@gmail.com'
+    role_id = 1
+    password = '123'
+    password = pbkdf2_sha256.hash(password)
+    user = User(name=name,username=username, email=email, password=password, role_id=role_id)
+    
+    db.session.add(user)
+    db.session.commit()
+
+    print("Users seeder executed successfully!")
