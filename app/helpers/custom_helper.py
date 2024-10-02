2a51f95240128afc994321460ce66279e0c2fc3b
saepulfariz <saepulfariz108@gmail.com>
2024-10-02 17:11:17 +0700

    add function setAlert and initAlert

--- app/helpers/custom_helper.py
+++ app/helpers/custom_helper.py
@@ -1,4 +1,5 @@
 import datetime
+from flask import render_template, redirect, url_for, request, abort, session, flash
 
 def format_date(date):
     """Helper function untuk memformat tanggal ke format DD-MM-YYYY."""
@@ -7,3 +8,20 @@ def format_date(date):
 def greet_user(name):
     """Helper function untuk menyapa user."""
     return f'Hello, {name}!'
+
+
+def setAlert(icon = 'success', title = 'Success', text = 'Test') :
+    # session['iconFlash'] = icon
+    # session['titleFlash'] = title
+    # session['textFlash'] = text
+    flash(icon, 'icon')
+    flash(title, 'title')
+    flash(text, 'text')
+
+def initAlert():
+    icon = session['iconFlash'] if 'iconFlash' in session else ''
+    title = session['titleFlash'] if 'titleFlash' in session else ''
+    text = session['textFlash'] if 'textFlash' in session else ''
+    load = '<div id="flash" data-icon="' + icon + '" data-title="' + title + '" data-text="' + text + '" data-url=""></div>'
+    print(load)
+    return load
