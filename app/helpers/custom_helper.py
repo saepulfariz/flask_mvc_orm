00f0b82fcab0651a7790599f0c07055439528055
saepulfariz <saepulfariz108@gmail.com>
2024-10-03 09:01:07 +0700

    add script in function initAlert

--- app/helpers/custom_helper.py
+++ app/helpers/custom_helper.py
@@ -26,6 +26,86 @@ def initAlert():
     icon = session['iconFlash'] if 'iconFlash' in session else ''
     title = session['titleFlash'] if 'titleFlash' in session else ''
     text = session['textFlash'] if 'textFlash' in session else ''
-    load = '<div id="flash" data-icon="' + icon + '" data-title="' + title + '" data-text="' + text + '" data-url=""></div>'
-    print(load)
+    load = '<div id="flash" data-icon="' + icon + '" data-title="' + title + '" data-text="' + text + '" data-url=""></div> '
+
+    load = load + """ <script>
+
+                    const Swal2 = Swal.mixin({
+                        customClass: {
+                        input: 'form-control'
+                        }
+                    })
+                    
+                    const Toast = Swal.mixin({
+                        toast: true,
+                        position: 'top-end',
+                        showConfirmButton: false,
+                        timer: 3000,
+                        timerProgressBar: true,
+                        didOpen: (toast) => {
+                        toast.addEventListener('mouseenter', Swal.stopTimer)
+                        toast.addEventListener('mouseleave', Swal.resumeTimer)
+                        }
+                    })
+                    
+                    
+                    function deleteTombol(e){
+                        const ket = e.getAttribute('data-ket');
+                        const href = e.getAttribute('data-href') ? e.getAttribute('data-href') : e.getAttribute('href');
+                        Swal.fire({
+                        title: 'Are you sure?',
+                        text: ket,
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Yes, delete it!'
+                        }).then((result) => {
+                        if (result.value) {
+                            if(href){
+                            window.location.href = href;
+                            }else{
+                            e.parentElement.submit();
+                            }
+                        }
+                        })
+                        e.preventDefault();
+                    }
+                    
+                    const iconFlash = document.getElementById('flash').getAttribute('data-icon');
+                    const titleFlash = document.getElementById('flash').getAttribute('data-title');
+                    const textFlash = document.getElementById('flash').getAttribute('data-text');
+                    const urlFlash = document.getElementById('flash').getAttribute('data-url');
+                    const typeFlash = document.getElementById('flash').getAttribute('data-type');
+
+
+                    if(typeFlash == 'sweetalert'){
+                        
+                        if (iconFlash && urlFlash) {
+                        Swal.fire({
+                            icon: iconFlash,
+                            title: titleFlash,
+                            text: textFlash
+                        }).then((result) => {
+                            if (result.value) {
+                            window.location.href = urlFlash;
+                            }
+                        })
+                        } else if (iconFlash) {
+                        Swal.fire({
+                            icon: iconFlash,
+                            title: titleFlash,
+                            text: textFlash
+                        })
+                        }
+
+                    }else if(typeFlash =='toast'){
+                        Toast.fire({
+                        icon: iconFlash,
+                        title: titleFlash
+                        })
+                    }
+
+                    </script> """
+    # print(load)
     return load
