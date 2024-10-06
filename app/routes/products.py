429885abfb78ff1e871d5a26800d13c6ad4bbea6
saepulfariz <saepulfariz108@gmail.com>
2024-10-06 23:38:23 +0700

    add login required routes products

--- app/routes/products.py
+++ app/routes/products.py
@@ -1,29 +1,36 @@
 from flask import Blueprint
-from app.controllers import product_controller,home_controller,user_controller
+from app.controllers import product_controller
+from app.middleware.auth import login_required
 
 products = Blueprint('products', __name__)
 
 
 @products.route('/products', methods=['GET'])
+@login_required
 def index():
     return product_controller.index()
 
 @products.route('/products/new', methods=['GET'])
+@login_required
 def new():
     return product_controller.new()
 
 @products.route('/products', methods=['POST'])
+@login_required
 def create():
     return product_controller.create()
 
 @products.route('/products/<int:id>/edit', methods=['GET'])
+@login_required
 def edit(id):
     return product_controller.edit(id)
 
 @products.route('/products/<int:id>',  methods=['PUT', 'PATCH'])
+@login_required
 def update(id):
     return product_controller.update(id)
 
 @products.route('/products/<int:id>', methods=['DELETE'])
+@login_required
 def delete(id):
     return product_controller.delete(id)
