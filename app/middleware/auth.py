e0bc65b87723da7512c86ddf75f1c3589201879d
saepulfariz <saepulfariz108@gmail.com>
2024-10-08 15:22:40 +0700

    custom forbidden access user

--- app/middleware/auth.py
+++ app/middleware/auth.py
@@ -1,6 +1,7 @@
 from functools import wraps
 from flask import Flask, request, redirect, url_for, session, abort
 from app.models import User
+from app.helpers.custom_helper import setAlert
 
 def login_required(role=None):
     def wrapper(f):
@@ -9,14 +10,14 @@ def login_required(role=None):
             if 'id' not in session:
                 return redirect(url_for('auth.index'))
             
-            id = session['id']
-            
-            user_data = User.query.get_or_404(id)
+            user_data = User.query.get_or_404(session['id'])
             if user_data is None:
                 return redirect(url_for('auth.index'))
             
             if role and user_data.role_id != role:
-                return abort(403)  # Forbidden access
+                # return abort(403)  # Forbidden access
+                setAlert('warning', 'Warning', 'Forbidden access')
+                return redirect(url_for('dashboard.index'))
             
             return f(*args, **kwargs)
         return decorated_function
