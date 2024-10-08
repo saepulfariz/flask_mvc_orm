afe285ae66e2113c630923b7c65ddb6cb0252c13
saepulfariz <saepulfariz108@gmail.com>
2024-10-08 17:15:55 +0700

    add login required in route dashboard

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,9 +1,11 @@
 from flask import Blueprint
 from app.controllers import dashboard_controller
+from app.middleware.auth import login_required
 
 dashboard = Blueprint('dashboard', __name__)
 
 
 @dashboard.route('/')
+@login_required()
 def index():
     return dashboard_controller.index()
